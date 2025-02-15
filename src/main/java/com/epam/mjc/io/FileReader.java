f81f69ebbc683cb4db026fb0034213d9337e24fe
Dzmitry <amigo1044@yandex.ru>
2025-02-15 20:34:35 +0300

    f

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String name = null;
+            Integer age = null;
+            String email = null;
+            Long phone = null;
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("Name: ")) {
+                    name = line.replace("Name: ", "");
+                } else if (line.startsWith("Age: ")) {
+                    try {
+                        age = Integer.parseInt(line.replace("Age: ", ""));
+                    } catch (NumberFormatException e) {
+                    }
+                } else if (line.startsWith("Email: ")) {
+                    email = line = line.replace("Email: ", "");
+                } else if (line.startsWith("Phone: ")) {
+                    try {
+                        phone = Long.parseLong(line.replace("Phone: ", ""));
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+            }
+            return new Profile(name, age, email, phone);
+        }
+        catch (IOException e) {
+            return new Profile();
+        }
     }
 }
